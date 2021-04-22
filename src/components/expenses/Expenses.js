87aa8a652f3cd659a003bbb08787aaed9edd7619
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-04-22 18:14:48 -0400

    changed function syntax @tanmaik

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../ui/Card";
 
-function Expenses(props) {
+const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpenseItem
@@ -27,6 +27,6 @@ function Expenses(props) {
       />
     </Card>
   );
-}
+};
 
 export default Expenses;
