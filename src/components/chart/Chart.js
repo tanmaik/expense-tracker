dd226e5744ce6f50ce517074ba3991622a719ff0
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-04-29 19:40:54 -0400

    finished chart and chartbar js

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -4,7 +4,14 @@ import "./Chart.css";
 const Chart = (props) => {
   return (
     <div className="chart">
-      <ChartBar></ChartBar>
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
     </div>
   );
 };
