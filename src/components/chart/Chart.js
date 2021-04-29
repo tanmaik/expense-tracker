4d13ba931119d4b91e215875052b8c536f6bc450
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2021-04-29 19:24:56 -0400

    changed chart and chartbar

---
+++ src/components/chart/Chart.js
@@ -0,0 +1,12 @@
+import ChartBar from "./ChartBar";
+import "./Chart.css";
+
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      <ChartBar></ChartBar>
+    </div>
+  );
+};
+
+export default Chart;
